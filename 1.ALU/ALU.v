1f7476028b8428632d8f6244395ce5dd4b1f58f1
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2022-02-09 21:55:08 +0530

    added the implementation of Arithmatic and Logic Unit

---
+++ 1.ALU/ALU.v
@@ -0,0 +1,81 @@
+module testbench;										//initializing the testbench for testing the alu
+	reg [7:0] OPERAND1,OPERAND2;						//initializing the registers to test the output
+	reg [2:0] ALUOP;
+	wire [7:0] ALURESULT; 								//initializing a wire to hold the output
+
+	alu myalu(OPERAND1,OPERAND2,ALURESULT,ALUOP);		//instantiating the alu module
+
+	initial 											//testing the outputs with intervals of #5
+	begin
+		OPERAND1 = 25;
+		OPERAND2 = 41;
+		ALUOP = 3'b000;
+		#10
+		OPERAND1 = 152;
+		OPERAND2 = 23; 
+		ALUOP = 3'b000;
+		#10 
+		OPERAND1 = 27;
+		OPERAND2 = 74;
+		ALUOP = 3'b001;
+		#10 
+		OPERAND1 = 96;
+		OPERAND2 = 4;
+		ALUOP = 3'b010;
+		#10 
+		OPERAND1 = 14;
+		OPERAND2 = 14;
+		ALUOP = 3'b011;
+		#10 ALUOP = 3'b100;
+		#10 ALUOP = 3'b101;
+		#10 OPERAND1 = 53;
+		OPERAND2 = 64;
+		#10 ALUOP = 3'b000;
+		#10 ALUOP = 3'b001;
+		#10 ALUOP = 3'b010;
+		#10 ALUOP = 3'b011;
+		#10 ALUOP = 3'b001;
+		#10 OPERAND2 = 22;
+		#10 OPERAND1 = 222;
+	end
+
+	initial												//opeining the initial block for displaying outputs with corresponding inputs
+	begin	
+		$display("|\tTime\t|\t    DATA1  \t|\t    DATA2\t|\t    SELECTION\t|\t    OUTPUT\t|");
+		$display("-----------------------------------------------------------------------------------------------------------------");
+		//monitoring the values
+		$monitor("|\t%g\t|\t%b (%d)\t|\t%b (%d)\t|\t      %b\t|\t%b (%d)\t|",$time,OPERAND1,OPERAND1,OPERAND2,OPERAND2,ALUOP,ALURESULT,ALURESULT);
+		//$dumpfile ("wavedata.vcd"); 					//uncomment if wavedata is needed
+    	//$dumpvars(0,myalu);							//uncomment if wavedata is needed
+		#150 $finish;									//finish at time units 100
+		
+	end
+	
+endmodule
+
+//this module contains the design of the alu unit
+module alu(DATA1, DATA2, RESULT, SELECT);		//declaring the name and the terminals of the unit
+
+	input [7:0] DATA1,DATA2;					//DATA1 and DATA2 are 8 bit inputs 
+	input [2:0] SELECT;							//SELECT is 3 bit input
+	output reg [7:0] RESULT;					//output(RESULT) is a 8 bit number
+	wire [7:0] FWwire,ADDwire,ANDwire,ORwire;	//declaring 4 wires to send the signals to the mux
+	
+	FORWARD_Module myFORWARD(DATA2,FWwire);		//instantiating the forwarding module of the alu
+	ADD_Module myADD(DATA1,DATA2,ADDwire);		//instantiating the adder of the alu
+	AND_Module myAND(DATA1,DATA2,ANDwire);		//instantiating the anding module of the alu
+	OR_Module myOR(DATA1,DATA2,ORwire);			//instantiating the oring module of the alu
+	
+	always @ (*)								//declaring the always block.since this is a case statement,sensitivity list is not defined
+	begin
+		case(SELECT)							//declaring the case statement based on the SELECT signal
+			0:RESULT=FWwire;					//if SELECT==3'b000 choose the output of the forwarding unit as the output of the alu
+			1:RESULT=ADDwire;					//if SELECT==3'b001 choose the output of the adder unit as the output of the alu
+			2:RESULT=ANDwire;					//if SELECT==3'b010 choose the output of the ANDing unit as the output of the alu
+			3:RESULT=ORwire;					//if SELECT==3'b000 choose the output of the ORing unit as the output of the alu
+												//notice that default case is not defined here.therefor whenever the SELECT>=4 output stays at the last value
+		endcase
+	
+	end
+	
+endmodule
---
+++ 1.ALU/OR.v
@@ -0,0 +1,48 @@
+/*
+module testbench;
+
+	reg [7:0] DATA1,DATA2;
+	wire [7:0] RESULT;
+
+	OR_Module myOR(DATA1,DATA2,RESULT);
+
+	initial 
+	begin
+		$monitor($time," DATA1:%b DATA2:%b RESULT:%b",DATA1,DATA2,RESULT);
+	end
+	
+	initial
+	begin
+		
+		DATA1 = 8'b0000_0111;
+		DATA2 = 8'b0000_0100;
+		
+		#1;
+		
+		DATA2 = 8'b0000_0101;
+		
+		#1;
+		
+		DATA1 = 8'b0000_1000;
+		DATA2 = 8'b0000_1100;
+		
+		
+	end
+
+endmodule
+*/
+
+//this module is supposed to get operand 1 and 2 as inputs and output the bitwise OR value of them
+module OR_Module(DATA1,DATA2,RESULT);		//declaring the module name and terminal of the module
+
+	input [7:0] DATA1,DATA2;				//inputs are 8 bit numbers
+	output reg [7:0] RESULT;				//output is a 8 bit number
+	
+	always @ (DATA1,DATA2)					//initialig the always block.This always block is triggered when DATA1 and DATA2 change
+	begin
+	
+		#1 RESULT =  DATA1 | DATA2;			//make the output of the module equal to bitwise or of DATA1 and DATA2 with 1 time unit delay
+	
+	end
+
+endmodule
